acd2abc65d079fc2ea875d05a17c36317a7213f0
Volodymyr <volodymyrzavada@gmail.com>
2022-01-06 16:59:23 -0500

    #18-1

--- src/main/java/com/example/testpipline/TestEndpoint.java
+++ src/main/java/com/example/testpipline/TestEndpoint.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.ZonedDateTime;
+
 @RestController
 @RequestMapping("/users")
 public class TestEndpoint {
 
     @GetMapping
     public String home() {
-        return "Users working... OK";
+        return "Users working... OK. " + ZonedDateTime.now();
     }
 }
